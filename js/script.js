5db89a991d7c3b016bf6fe214f6689efd895b483
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-16 16:03:18 +0300

    updating project primery theme color and adding typing animation in home

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* typing animation */
+
+const typed = new Typed(".typing", {
+    strings: ["", "Software Engineer Jr.", "Front-end Developer.","Web Developer.", "Back-end Developer.", "Python Developer."],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
