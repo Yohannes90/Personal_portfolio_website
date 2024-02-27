0ec31042fc618ad9b52383665792b0b903b6465f
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-27 04:28:15 +0300

    checking and fixing style-switcher.js file code to be semistandard compliant

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,48 +1,47 @@
 /* toggle style switcher  */
 
-const styleSwicherToggler = document.querySelector(".style-switcher-toggler");
-styleSwicherToggler.addEventListener("click", () => {
-    document.querySelector(".style-switcher").classList.toggle("open");
-})
+const styleSwicherToggler = document.querySelector('.style-switcher-toggler');
+styleSwicherToggler.addEventListener('click', () => {
+  document.querySelector('.style-switcher').classList.toggle('open');
+});
 
 // hide style - switcher on scroll
-window.addEventListener("scroll", () => {
-    if (document.querySelector(".style-switcher").classList.contains("open")) {
-        document.querySelector(".style-switcher").classList.remove("open");
-    }
-})
+window.addEventListener('scroll', () => {
+  if (document.querySelector('.style-switcher').classList.contains('open')) {
+    document.querySelector('.style-switcher').classList.remove('open');
+  }
+});
 
 /* theme colors */
 
-const alternateStyle = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) {
-    localStorage.setItem("color", color);
-    changeColor();
+const alternateStyle = document.querySelectorAll('.alternate-style');
+function setActiveStyle (color) {
+  localStorage.setItem('color', color);
+  changeColor();
 }
 
-function changeColor() {
-    alternateStyle.forEach((style) => {
-        if (localStorage.getItem("color") === style.getAttribute("title")) {
-            style.removeAttribute("disabled")
-        } else {
-            style.setAttribute("disabled", "true")
-        }
-    })
-};
+function changeColor () {
+  alternateStyle.forEach((style) => {
+    if (localStorage.getItem('color') === style.getAttribute('title')) {
+      style.removeAttribute('disabled');
+    } else {
+      style.setAttribute('disabled', 'true');
+    }
+  });
+}
 
 /* theme light and dark mode */
 
-const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
-})
-window.addEventListener("load", () => {
-    if (document.classList.contains("dark")) {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    } else {
-        dayNight.querySelector("i").classList.add("fa-moon");
-
-    }
-})
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', () => {
+  dayNight.querySelector('i').classList.toggle('fa-sun');
+  dayNight.querySelector('i').classList.toggle('fa-moon');
+  document.body.classList.toggle('dark');
+});
+window.addEventListener('load', () => {
+  if (document.classList.contains('dark')) {
+    dayNight.querySelector('i').classList.add('fa-sun');
+  } else {
+    dayNight.querySelector('i').classList.add('fa-moon');
+  }
+});
